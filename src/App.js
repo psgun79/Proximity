ea718da4c023dcbb9cdf83417f977f6beef13d4b
unknown <psgun79@kaist.ac.kr>
2021-11-23 14:49:54 +0900

    Changed video link

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const YoutubeEmbed = ({ embedId }) => (
     <iframe
       width="1280"
       height="720"
-      src={`https://www.youtube.com/embed/svKUJDNEgho`}
+      src={`https://www.youtube.com/embed/smVIpKFUcWU`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
